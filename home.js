a5cac3ae39a5a58af2f9ae905ac7e04996618e89
Carlos Bejar <cbejar93@gmail.com>
2025-07-01 13:15:30 -0700

    mobile first, cards, personal info

--- home.js
+++ home.js
@@ -1,31 +1,36 @@
 function toggleDarkMode() {
-    const body = document.body;
-    const icon = document.getElementById('theme-icon');
-    body.classList.toggle('dark-mode');
-    if (body.classList.contains('dark-mode')) {
-      icon.classList.remove('fa-moon');
-      icon.classList.add('fa-sun');
-    } else {
-      icon.classList.remove('fa-sun');
-      icon.classList.add('fa-moon');
-    }
+  const body = document.body;
+  const icon = document.getElementById('theme-icon');
+  const toggleText = document.querySelector('.toggle-btn');
+  body.classList.toggle('dark-mode');
+  if (body.classList.contains('dark-mode')) {
+    icon.classList.remove('fa-moon');
+    icon.classList.add('fa-sun');
+    toggleText.innerHTML = '<i id="theme-icon" class="fas fa-sun"></i> Toggle Light Mode';
+  } else {
+    icon.classList.remove('fa-sun');
+    icon.classList.add('fa-moon');
+    toggleText.innerHTML = '<i id="theme-icon" class="fas fa-moon"></i> Toggle Dark Mode';
   }
+}
 
-  document.querySelectorAll('.collapsible').forEach(button => {
-    button.addEventListener('click', () => {
-      const content = button.nextElementSibling;
-      button.classList.toggle('active');
-      content.classList.toggle('collapsed');
-      if (!content.classList.contains('collapsed')) {
-        content.style.maxHeight = content.scrollHeight + 'px';
-      } else {
-        content.style.maxHeight = null;
-      }
-    });
+document.querySelectorAll('.collapsible').forEach(button => {
+  button.addEventListener('click', () => {
+    const content = button.nextElementSibling;
+    button.classList.toggle('active');
+    content.classList.toggle('collapsed');
+    if (!content.classList.contains('collapsed')) {
+      content.style.maxHeight = content.scrollHeight + 'px';
+    } else {
+      content.style.maxHeight = null;
+    }
   });
+});
 
-  // Initialize collapsed state
-  document.querySelectorAll('.collapsible-content').forEach(content => {
-    content.classList.add('collapsed');
-    content.style.maxHeight = 0;
-  });
+// Initialize collapsed state
+document.querySelectorAll('.collapsible').forEach(button => {
+  const content = button.nextElementSibling;
+  button.classList.add('active');
+  content.classList.remove('collapsed');
+  content.style.maxHeight = content.scrollHeight + 'px';
+});
